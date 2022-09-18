8156909cbb5f76527f7fe8d03025f936eedc4431
muneeb706 <muneeb706@gmail.com>
2022-09-17 19:18:35 -0700

    config (package): init files - core application package init file added - this contains the factory method to create flask app and to initialize db

--- core/__init__.py
+++ core/__init__.py
@@ -1,27 +1,47 @@
-import os
+from typing import Optional
 
 from flask import Flask
+from core import config
+import click
+from .db import Database
+import os
+
+db = Database.get_instance()
 
 
-def create_app(test_config=None):
+@click.command('init-db')
+def init_db_command():
+    try:
+        os.makedirs(f"{config.TMP_DIR}")
+    except OSError:
+        pass
+    """Clear the existing data and create new tables."""
+    open(f"{config.DB_FILE}", "w")
+    Database.init_db()
+    click.echo('Initialized the database.')
+
+
+def create_app(cfg: Optional[config.AppConfig] = None):
+    if cfg is None:
+        cfg = config.AppConfig()
+
     # create and configure the app
     # instance_relative_config=True tells the app that
     # configuration files are relative to the instance folder.
     # The instance folder is located outside the core package
     # and can hold local data that shouldn’t be committed to
     # version control, such as configuration secrets and the database file.
-    app = Flask(__name__, instance_relative_config=True)
-
     # ensure the instance folder exists
-    try:
-        os.makedirs("tmp")
-    except OSError:
-        pass
 
-    from . import db
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(cfg)
+
     db.init_app(app)
 
     from . import views
     app.register_blueprint(views.bp)
+
+    app.teardown_appcontext(Database.close_db)
+    app.cli.add_command(init_db_command)
 
     return app
