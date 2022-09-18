556528d3ed1a72f9383aac25d4f76cc155083cce
muneeb706 <muneeb706@gmail.com>
2022-09-17 19:16:46 -0700

    feat (controller): Pet - controller to interact with Pet model added

--- core/controller.py
+++ core/controller.py
@@ -1,6 +1,6 @@
-from .models import Owner
+from .models import Owner, Pet
 from .db import Database
-import uuid
+from .utils import get_uuid_str
 
 
 class OwnerController:
@@ -8,15 +8,32 @@ class OwnerController:
     @classmethod
     def __generate_username(cls, fullname):
         username = fullname.lower().replace(' ', '')
-        username += str(uuid.uuid4())[:5]
+        username += get_uuid_str()[:5]
         return username
 
     def get_owner(self, identifier):
         owner_q = Owner.query.filter((Owner.fullname == identifier) | (Owner.username == identifier)).first()
         return owner_q
 
+    def get_pets(self, username):
+        owner_q = Owner.query.filter(Owner.username == username).first()
+        return owner_q.pets
+
     def insert_owner(self, fullname):
         username = self.__generate_username(fullname)
         owner_o = Owner(username=username, fullname=fullname)
         Database.insert(owner_o)
         return self.get_owner(username)
+
+
+class PetController:
+
+    def insert(self, pet_type, owner_name):
+        pet_o = Pet(serial_number=get_uuid_str(), owner_name=owner_name, type=pet_type)
+        Database.insert(pet_o)
+
+    @staticmethod
+    def get_all():
+        return Pet.query.all()
+
+
