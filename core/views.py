c25857f96d1f7cba123d2cbbe718b0dab4d8adb2
muneeb706 <muneeb706@gmail.com>
2022-09-18 10:46:37 -0700

    refactor (views): Views - comments added - unnecessary object creation removed

--- core/views.py
+++ core/views.py
@@ -1,12 +1,27 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .controller import OwnerController, PetController
-from .config import ADMIN_NAME
+"""
+Contains definition and business logic for each route handled by the package
+"""
 
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from .config import ADMIN_NAME
+from .controller import OwnerController, PetController
+
+# Blueprints in flask are used to de-couple application into smaller
+# re-usable components. Blueprints are registered with the flask
+# application
 bp = Blueprint('view', __name__, url_prefix='/')
 
 
 @bp.route("/", methods=["GET", "POST"])
 def welcome():
+    """
+    GET: Returns welcome.html template
+    POST: Inserts owner with the given fullname in the database
+    if it does not exist. Then, redirects the owner to the home page
+    (home.html). If given fullname belongs to the admin then redirects to
+    admin page (admin.html)
+    """
     if request.method == "POST":
         fullname = request.form.get('fullname')
 
@@ -14,41 +29,44 @@ def welcome():
             return redirect(url_for('view.admin'))
 
         fullname_stripped = fullname.strip()
-        owner_controller = OwnerController()
-        owner_o = owner_controller.get_owner(fullname_stripped)
+        owner_o = OwnerController.get_owner(fullname_stripped)
         if not owner_o:
-            owner_o = owner_controller.insert_owner(fullname_stripped)
+            owner_o = OwnerController.insert_owner(fullname_stripped)
         return redirect(url_for('view.home', username=owner_o.username))
 
     return render_template("welcome.html")
 
 
-@bp.route("/admin")
+@bp.route("/admin", methods=["GET"])
 def admin():
-    pet_controller = PetController()
-    pets = pet_controller.get_all()
+    """
+    Returns admin page, with the list of all the pets
+    with their owners.
+    """
+
+    pets = PetController.get_all()
     return render_template("admin.html", pets=[pet.serialize for pet in pets])
 
 
-@bp.route("/home/<username>")
+@bp.route("/home/<username>", methods=["GET"])
 def home(username):
-    controller = OwnerController()
-    pets = controller.get_pets(username)
+    """
+    Returns home page, with list of all the pets belonging to the
+    owner identified by given username.
+    """
+    pets = OwnerController.get_pets(username)
     return render_template("home.html", username=username, pets=[pet.serialize for pet in pets])
 
 
 @bp.route("/buy", methods=["POST"])
 def buy_pet():
+    """
+    Inserts pets recorded with the owner who bought the pet in the database
+    """
+
     pet_type = request.form.get('pet')
     owner_username = request.form.get('username')
-    owner_controller = OwnerController()
-    owner_o = owner_controller.get_owner(identifier=owner_username)
-
-    if owner_o:
-        pet_controller = PetController()
-        pet_controller.insert(pet_type, owner_o.fullname)
+    owner_o = OwnerController.get_owner(identifier=owner_username)
+    PetController.insert(pet_type, owner_o.fullname)
 
     return redirect(url_for('view.home', username=owner_username))
-
-
-
