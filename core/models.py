231478622778cc55fe30d8556eba3b9f03ded301
muneeb706 <muneeb706@gmail.com>
2022-09-17 19:14:46 -0700

    feat (models): Pet - serialize property added

--- core/models.py
+++ core/models.py
@@ -16,3 +16,12 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     serial_number = db.Column(db.String, unique=True, nullable=False)
     owner_name = db.Column(db.String, db.ForeignKey('owner.fullname'), nullable=True)
+    type = db.Column(db.String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'type': self.type,
+            'serial_number': self.serial_number,
+            'owner': self.owner_name
+        }
