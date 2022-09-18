984da321a6163068b946dfbf0665dfa543c32223
muneeb706 <muneeb706@gmail.com>
2022-09-17 19:17:25 -0700

    config (Config): DB File Path - DB file path assigned to variable

--- core/config.py
+++ core/config.py
@@ -1,13 +1,15 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+TMP_DIR = f"{ROOT_DIR}/tmp"
+DB_FILE = f"{TMP_DIR}/pet-marketplace.db"
+ADMIN_NAME = "administrator"
 
 
-class Config:
+class AppConfig:
     DEBUG = True
     DEVELOPMENT = True
     SECRET_KEY = "2c084ae6f1b340a7af8880400b83cb49"
     FLASK_SECRET = SECRET_KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{ROOT_DIR}/tmp/pet-marketplace.db"
-
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_FILE}"
