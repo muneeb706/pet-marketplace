5f2b29bf0213970f254a74eebd8ba5a5538ac7dd
muneeb706 <muneeb706@gmail.com>
2022-09-18 14:57:34 -0700

    test (controller): Refactor - application context managed in base class

--- core/tests/test_controller.py
+++ core/tests/test_controller.py
@@ -14,46 +14,48 @@ class TestBase(TestCase):
         # See https://martin-thoma.com/sql-connection-strings/
         self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
 
-        with self.app.app_context():
-            Database.init_db()
+        # adding flask application context
+        # so, that we don't have to open context
+        # every time we interact with the application
+        self._ctx = self.app.test_request_context()
+        self._ctx.push()
+        Database.init_db()
 
     def tearDown(self):
-        with self.app.app_context():
-            Database.close_db()
+        Database.close_db()
+        if getattr(self, '_ctx') and self._ctx is not None:
+            self._ctx.pop()
+        del self._ctx
 
 
 class TestOwnerController(TestBase):
 
     def test_insert_owner(self):
-        with self.app.app_context():
-            owner_o = OwnerController.insert_owner("Muneeb Shahid")
-            self.assertIsNotNone(owner_o)
-            self.assertIsNotNone(owner_o.username)
+        owner_o = OwnerController.insert_owner("Muneeb Shahid")
+        self.assertIsNotNone(owner_o)
+        self.assertIsNotNone(owner_o.username)
 
     def test_get_pets(self):
-        with self.app.app_context():
-            # inserting pet with owner
-            owner_o = OwnerController.insert_owner("Muneeb Shahid")
-            pet_o = PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
-            pets = OwnerController.get_pets(owner_o.username)
-            self.assertEqual(1, len(pets))
-            self.assertEqual(pet_o.serial_number, pets[0].serial_number)
+        # inserting pet with owner
+        owner_o = OwnerController.insert_owner("Muneeb Shahid")
+        pet_o = PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
+        pets = OwnerController.get_pets(owner_o.username)
+        self.assertEqual(1, len(pets))
+        self.assertEqual(pet_o.serial_number, pets[0].serial_number)
 
 
 class TestPetController(TestBase):
 
     def test_insert_pet(self):
-        with self.app.app_context():
-            owner_o = OwnerController.insert_owner("Muneeb Shahid")
-            pet_o = PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
-            self.assertIsNotNone(pet_o)
-            self.assertEqual("dog", pet_o.type)
+        owner_o = OwnerController.insert_owner("Muneeb Shahid")
+        pet_o = PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
+        self.assertIsNotNone(pet_o)
+        self.assertEqual("dog", pet_o.type)
 
     def test_get_all(self):
-        with self.app.app_context():
-            owner_o = OwnerController.insert_owner("Muneeb Shahid")
-            PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
-            PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
+        owner_o = OwnerController.insert_owner("Muneeb Shahid")
+        PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
+        PetController.insert_pet(pet_type="dog", owner_name=owner_o.fullname)
 
-            pets = PetController.get_all()
-            self.assertEqual(2, len(pets))
+        pets = PetController.get_all()
+        self.assertEqual(2, len(pets))
