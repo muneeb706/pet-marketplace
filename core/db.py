460cb54c0860d974b30ca81f41851bffdd37d375
muneeb706 <muneeb706@gmail.com>
2022-09-17 21:19:43 -0700

    refactor (database): Database - comments added

--- core/db.py
+++ core/db.py
@@ -1,13 +1,24 @@
+"""
+Contains singleton class for Database instance.
+"""
+from typing import List
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 class Database:
+    """
+    Singleton class to interact with the database
+    """
 
     __instance = None
 
     @staticmethod
-    def get_instance():
-        """Static Access Method"""
+    def get_instance() -> SQLAlchemy:
+        """
+        Returns SQLAlchemy database instance.
+        Creates if not already initialized
+        """
         if not Database.__instance:
             Database()
         return Database.__instance
@@ -20,21 +31,37 @@ class Database:
 
     @classmethod
     def init_db(cls):
+        """
+        Initializes database by creating all the tables
+        defined in the models
+        """
         cls.get_instance().create_all()
 
     @classmethod
-    def insert(cls, model_instance):
+    def insert(cls, model_instance: SQLAlchemy.Model):
+        """
+        Inserts record in the database table as
+        represented by model instance
+        :param model_instance: Instance of any model that needs to be inserted
+        """
+
         cls.get_instance().session.add(model_instance)
         cls.get_instance().session.commit()
 
     @classmethod
-    def insert_all(cls, model_instances):
+    def insert_all(cls, model_instances: List[SQLAlchemy.Model]):
+        """
+        Inserts multiple records in the database as
+        represented by list of model instances
+        :param model_instances: List of model instances
+        """
+
         cls.get_instance().session.add_all(model_instances)
         cls.get_instance().session.commit()
 
     @classmethod
     def close_db(cls, e=None):
+        """
+        Terminates database connection
+        """
         cls.get_instance().session.close()
-
-
-
