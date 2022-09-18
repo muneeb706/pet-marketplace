d3ca29196a567b8348b29881d82999a083670db4
muneeb706 <muneeb706@gmail.com>
2022-09-17 20:48:35 -0700

    refactor (core): Comments - comments added

--- core/__init__.py
+++ core/__init__.py
@@ -1,47 +1,65 @@
+"""
+It contains flask application factory and commands to initialize database
+"""
+
+import os
 from typing import Optional
 
-from flask import Flask
-from core import config
 import click
-from .db import Database
-import os
+from flask import Flask
 
+from core import config
+from .db import Database
+
+# Singleton SQLAlchemy db instance
 db = Database.get_instance()
 
 
 @click.command('init-db')
 def init_db_command():
+    """
+    This method runs against the init-db command.
+    It initializes the database.
+    """
+
+    # create tmp directory if it does not exist
     try:
         os.makedirs(f"{config.TMP_DIR}")
     except OSError:
         pass
-    """Clear the existing data and create new tables."""
+
+    # create db file if not exists
     open(f"{config.DB_FILE}", "w")
+
     Database.init_db()
-    click.echo('Initialized the database.')
+
+    click.echo('Database setup complete.')
 
 
-def create_app(cfg: Optional[config.AppConfig] = None):
+def create_app(cfg: Optional[config.AppConfig] = None) -> Flask:
+    """
+    Creates and returns flask application after configuring it
+    :param cfg: config object
+    :return: flask application
+    """
+
     if cfg is None:
         cfg = config.AppConfig()
 
-    # create and configure the app
-    # instance_relative_config=True tells the app that
-    # configuration files are relative to the instance folder.
-    # The instance folder is located outside the core package
-    # and can hold local data that shouldn’t be committed to
-    # version control, such as configuration secrets and the database file.
-    # ensure the instance folder exists
-
     app = Flask(__name__, instance_relative_config=True)
+    # update the config values from given object
     app.config.from_object(cfg)
 
+    # Linking SQLAchemy db object with flask application
     db.init_app(app)
 
+    # registering views module with the application
     from . import views
     app.register_blueprint(views.bp)
 
+    # function to be called when application context ends
     app.teardown_appcontext(Database.close_db)
+    # adding cli commands to application
     app.cli.add_command(init_db_command)
 
     return app
